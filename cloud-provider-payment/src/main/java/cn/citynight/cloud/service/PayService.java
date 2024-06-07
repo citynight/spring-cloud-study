d58badc07551a13bc3a1ebfa717d730b034c04af
Logan <lixzzzzzz@126.com>
2024-06-07 18:32:42 +0800

    wip: 实现 service

---
+++ cloud-provider-payment/src/main/java/cn/citynight/cloud/service/PayService.java
@@ -0,0 +1,13 @@
+package cn.citynight.cloud.service;
+
+import cn.citynight.cloud.entities.Pay;
+
+import java.util.List;
+
+public interface PayService {
+    int add(Pay pay);
+    int delete(Integer id);
+    int update(Pay pay);
+    Pay getById(Integer id);
+    List<Pay> getAll();
+}
---
+++ cloud-provider-payment/src/main/java/cn/citynight/cloud/service/impl/PayServiceImpl.java
@@ -0,0 +1,39 @@
+package cn.citynight.cloud.service.impl;
+
+import cn.citynight.cloud.entities.Pay;
+import cn.citynight.cloud.mapper.PayMapper;
+import cn.citynight.cloud.service.PayService;
+import jakarta.annotation.Resource;
+
+import java.util.List;
+
+public class PayServiceImpl implements PayService {
+
+    @Resource
+    private PayMapper payMapper;
+
+    @Override
+    public int add(Pay pay) {
+        return payMapper.insertSelective(pay);
+    }
+
+    @Override
+    public int delete(Integer id) {
+        return payMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public int update(Pay pay) {
+        return payMapper.updateByExampleSelective(pay, null);
+    }
+
+    @Override
+    public Pay getById(Integer id) {
+        return payMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public List<Pay> getAll() {
+        return payMapper.selectAll();
+    }
+}
